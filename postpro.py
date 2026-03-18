3b5aa3840587e1d2bb48755db49f411b2b6e63fa
Kartikay Shukla <kartikayshukla@Kartikays-MacBook-Air-2.local>
2026-03-18 14:03:25 -0500

    Postprocessor function

---
+++ postpro.py
@@ -0,0 +1,31 @@
+'''
+Author:      Kartikay Shukla
+File:        postpro.py
+Created:     Oct 21, 2025 
+LM:          March 03, 2026
+
+DESCRIPTION
+This file has a function that stores the exact solution of problem which can be used to compare FEM solutions.
+It also has the post processing functions. It takes the solution from FEM function and prepares them for plotting. 
+'''
+import numpy as np
+from gen.gen_interpFunction import interpLagGLQ
+from gen.gen_utilities import rotVector, rotTensor
+
+ 
+def postprocessing(elemGlobalCoord, solution):
+    #---------------------Initialise----------------------#
+    # x-axis values for plotting
+    Zaxis = np.unique(elemGlobalCoord.flatten())
+    
+    # y-axis values for plotting
+    Defl = {'X': solution['0'],
+            'Y': solution['1'],
+            'Z': solution['2']
+            }
+    Slope = {'X': solution['3'],
+             'Y': solution['4'],
+             'Z': solution['5']
+             }
+
+    return Zaxis, Defl, Slope
